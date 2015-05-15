d68c96688b712ad1d2e6ec654bb30d130b41f556
Matthew Erbs <matthew.erbs@merbla.com>
2015-05-16 08:26:27 +1000

    Added simple sample to extend with generator

---
+++ sample/Serilog.Sinks.Splunk.Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.Splunk.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.LiterateConsole()
+                .WriteTo.SplunkViaTcp("localhost", 8089)
+                .CreateLogger();
+
+        }
+    }
+}
