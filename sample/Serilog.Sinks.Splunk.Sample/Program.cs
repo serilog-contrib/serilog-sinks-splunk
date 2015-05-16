8cc2046977b8bd3ad87bc24064fd95edb91167a2
Matthew Erbs <matthew.erbs@merbla.com>
2015-05-16 11:52:34 +1000

    Simple test

--- sample/Serilog.Sinks.Splunk.Sample/Program.cs
+++ sample/Serilog.Sinks.Splunk.Sample/Program.cs
@@ -10,12 +10,23 @@ namespace Serilog.Sinks.Splunk.Sample
         {
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.LiterateConsole()
-                .WriteTo.SplunkViaTcp("127.0.0.1", 10001)
+                .WriteTo.SplunkViaTcp("127.0.0.1", 10001, renderTemplate:true)
                 .CreateLogger();
 
-            Log.Information("Just another test");
+            var person = new Person() {DateOfBirth = DateTime.Now.AddYears(-30), FirstName = "Joe", Surname = "Bloggs"};
+
+            Log.Information("Just another test {@person}", person);
 
             Console.ReadLine();
         }
     }
+
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+
 }
--- src/Serilog.Sinks.Splunk.FullNetFx/Sinks/Splunk/SplunkViaUdpSink.cs
+++ src/Serilog.Sinks.Splunk.FullNetFx/Sinks/Splunk/SplunkViaUdpSink.cs
@@ -28,8 +28,8 @@ namespace Serilog.Sinks.Splunk
     /// </summary>
     public class SplunkViaUdpSink : ILogEventSink, IDisposable
     {
-        Socket _socket;
-        JsonFormatter _jsonFormatter;
+        readonly Socket _socket;
+        readonly JsonFormatter _jsonFormatter;
 
         /// <summary>
         /// Creates an instance of the Splunk UDP Sink
@@ -52,12 +52,14 @@ namespace Serilog.Sinks.Splunk
         /// <param name="host">The Splunk Host</param>
         /// <param name="port">The UDP port configured in Splunk</param>
         /// <param name="formatProvider">Optional format provider</param>
-        public SplunkViaUdpSink(string host, int port, IFormatProvider formatProvider = null)
+        /// <param name="renderTemplate">If true, the message template is rendered</param>
+        public SplunkViaUdpSink(string host, int port, IFormatProvider formatProvider = null,
+             bool renderTemplate = true)
         {
             _socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
             _socket.Connect(host, port);
 
-            _jsonFormatter = new JsonFormatter(renderMessage: true, formatProvider: formatProvider);
+            _jsonFormatter = new SplunkJsonFormatter(renderMessage: true, formatProvider: formatProvider, renderTemplate:renderTemplate);
         }
 
         /// <inheritdoc/>
