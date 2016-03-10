ba41f93a9bd13aad073d0e5f52cdc80971e80dc1
Matthew Erbs <matthew.erbs@merbla.com>
2016-03-10 20:00:55 +1000

    Sample app and corrected project.json targets

---
+++ src/sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading;
+using Serilog;
+using Serilog.Core;
+
+namespace Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+
+            Log.Logger = new LoggerConfiguration() 
+                .WriteTo.LiterateConsole() 
+                .CreateLogger();
+
+            Log.Information("Sample starting up");
+
+            foreach (var i in Enumerable.Range(0, 1000))
+            {
+                Log.Information("Running loop {Counter}", i);
+
+                Thread.Sleep(1000);
+                Log.Debug("Loop iteration done");
+            }
+        }
+    }
+}
