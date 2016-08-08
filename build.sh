8954089847cfbeeec8f79eb16883dbd0ac51a8bc
Matthew Erbs <matthew.erbs@merbla.com>
2016-08-08 16:51:10 +1000

    Rework of CI and added extra tests.

--- build.sh
+++ build.sh
@@ -5,6 +5,12 @@ for path in src/*/project.json; do
     dotnet build ${dirname}
 done 
 
+for path in test/*/project.json; do
+    dirname="$(dirname "${path}")"
+    dotnet build ${dirname} -f netcoreapp1.0
+    dotnet test ${dirname} -f netcoreapp1.0
+done 
+
 for path in sample/project.json; do
     dirname="$(dirname "${path}")"
     dotnet build ${dirname}
