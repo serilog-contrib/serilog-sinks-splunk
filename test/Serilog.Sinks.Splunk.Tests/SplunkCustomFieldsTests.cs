7d85d2a03c19499f9538176dfdbc17a8783892de
Patrik Lindström <patrik.lindstrom@lcube.se>
2017-06-06 23:42:55 +0200

    setting up Arrange part of CustomFields_for_Splunk_Sink test

--- test/Serilog.Sinks.Splunk.Tests/SplunkCustomFieldsTests.cs
+++ test/Serilog.Sinks.Splunk.Tests/SplunkCustomFieldsTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Serilog;
@@ -20,9 +21,11 @@ namespace Serilog.Sinks.Splunk.Tests
         const string SPLUNK_ENDPOINT = "http://ws2012-devops:8088"; //  Patrik local vm machine    
         const string SPLUNK_HEC_TOKEN = "1AFAC088-BFC6-447F-A358-671FA7465342"; // PATRIK lOCAL VM -MACHINE
         [Test]
-        void Test_Add_Extra_Splunk_Field_()
+        void Test_Add_CustomFields_for_Splunk_Sink_()
         {
             //Arrange
+            int a = 1;
+            int b = 0;
             var metaData = new CustomFields();
             metaData.CustomFieldList.Add(new CustomField("relChan", "Test"));
             metaData.CustomFieldList.Add(new CustomField("version", "17.8.9.beta"));
@@ -42,7 +45,14 @@ namespace Serilog.Sinks.Splunk.Tests
     .Enrich.WithProperty("Serilog.Sinks.Splunk.Sample.TestType", "AddCustomFields")
     .CreateLogger();
             //Act
-
+            try
+            {
+                var willnotwork = a / b;
+            }
+            catch (Exception e)
+            {
+              sut.Debug(e,"Should be an div by zeroerror");
+            }
             //Assert
 
         }
