e2a40400993c48cff417fba24bd5551e2ac16aff
fred2u <frederic.dehu@gmail.com>
2020-05-27 23:36:03 +0200

    Compatibility with .net standard

---
+++ src/Serilog.Sinks.TCP/Sinks/Splunk/SplunkTcpSinkConnectionInfo.cs
@@ -0,0 +1,66 @@
+﻿// Copyright 2016 Serilog Contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Net;
+
+namespace Serilog.Sinks.Splunk
+{
+    /// <summary>
+    /// Defines connection info used to connect against Splunk
+    /// using TCP.
+    /// </summary>
+    public class SplunkTcpSinkConnectionInfo
+    {
+        /// <summary>
+        /// Default size of the socket writer queue.
+        /// </summary>
+        public const int DefaultMaxQueueSize = 10000;
+
+        /// <summary>
+        /// Splunk host.
+        /// </summary>
+        public IPAddress Host { get; }
+
+        /// <summary>
+        /// Splunk port.
+        /// </summary>
+        public int Port { get; }
+
+        /// <summary>
+        /// Max Queue size for the TCP socket writer.
+        /// See <see cref="DefaultMaxQueueSize"/> for default value (10000).
+        /// </summary>
+        public int MaxQueueSize { get; set; } = DefaultMaxQueueSize;
+
+        /// <summary>
+        /// Creates an instance of <see cref="SplunkTcpSinkConnectionInfo"/> used
+        /// for defining connection info for connecting using TCP against Splunk.
+        /// </summary>
+        /// <param name="host">Splunk host.</param>
+        /// <param name="port">Splunk TCP port.</param>
+        public SplunkTcpSinkConnectionInfo(string host, int port) : this(IPAddress.Parse(host), port) { }
+
+        /// <summary>
+        /// Creates an instance of <see cref="SplunkTcpSinkConnectionInfo"/> used
+        /// for defining connection info for connecting using TCP against Splunk.
+        /// </summary>
+        /// <param name="host">Splunk host.</param>
+        /// <param name="port">Splunk TCP port.</param>
+        public SplunkTcpSinkConnectionInfo(IPAddress host, int port)
+        {
+            Host = host;
+            Port = port;
+        }
+    }
+}
--- src/Serilog.Sinks.TCP/Sinks/Splunk/TcpSink.cs
+++ src/Serilog.Sinks.TCP/Sinks/Splunk/TcpSink.cs
@@ -1,6 +1,4 @@
-﻿#if TCP
-
-// Copyright 2016 Serilog Contributors
+﻿// Copyright 2016 Serilog Contributors
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,14 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using System.IO;
-using System.Net;
-using System.Text;
 using Serilog.Core;
 using Serilog.Events;
 using Serilog.Formatting;
 using Splunk.Logging;
+using System;
+using System.IO;
+using System.Text;
 
 namespace Serilog.Sinks.Splunk
 {
@@ -30,7 +27,10 @@ namespace Serilog.Sinks.Splunk
     /// </summary>
     public class TcpSink : ILogEventSink, IDisposable
     {
-        readonly ITextFormatter _formatter;
+        private readonly ITextFormatter _formatter;
+        private readonly SplunkTcpSinkConnectionInfo _connectionInfo;
+        private bool disposedValue = false;
+
         private TcpSocketWriter _writer;
 
         /// <summary>
@@ -39,13 +39,9 @@ namespace Serilog.Sinks.Splunk
         /// <param name="connectionInfo">Connection info used for connecting against Splunk.</param>
         /// <param name="formatProvider">Optional format provider</param>
         /// <param name="renderTemplate">If true, the message template will be rendered</param>
-        public TcpSink(
-            SplunkTcpSinkConnectionInfo connectionInfo,
-            IFormatProvider formatProvider = null,
-            bool renderTemplate = true)
+        public TcpSink(SplunkTcpSinkConnectionInfo connectionInfo, IFormatProvider formatProvider = null, bool renderTemplate = true)
+            : this(connectionInfo, CreateDefaultFormatter(formatProvider, renderTemplate))
         {
-            _writer = CreateSocketWriter(connectionInfo);
-            _formatter = CreateDefaultFormatter(formatProvider, renderTemplate);
         }
 
         /// <summary>
@@ -53,56 +49,31 @@ namespace Serilog.Sinks.Splunk
         /// </summary>
         /// <param name="connectionInfo">Connection info used for connecting against Splunk.</param>
         /// <param name="formatter">Custom formatter to use if you e.g. do not want to use the JsonFormatter.</param>
-        public TcpSink(
-            SplunkTcpSinkConnectionInfo connectionInfo,
-            ITextFormatter formatter)
+        public TcpSink(SplunkTcpSinkConnectionInfo connectionInfo, ITextFormatter formatter)
         {
-            _writer = CreateSocketWriter(connectionInfo);
+            _connectionInfo = connectionInfo;
             _formatter = formatter;
+            _writer = CreateSocketWriter(connectionInfo);
         }
 
-        /// <summary>
-        /// Creates an instance of the Splunk TCP Sink
-        /// </summary>
-        /// <param name="host">The Splunk Host</param>
-        /// <param name="port">The TCP port configured in Splunk</param>
-        /// <param name="formatProvider">Optional format provider</param>
-        /// <param name="renderTemplate">If true, the message template will be rendered</param>
-        [Obsolete("Use the overload accepting a connection info object instead. This overload will be removed.", false)]
-        public TcpSink(
-            string host,
-            int port,
-            IFormatProvider formatProvider = null,
-            bool renderTemplate = true) : this(new SplunkTcpSinkConnectionInfo(host, port), formatProvider, renderTemplate)
+        /// <inheritdoc/>
+        protected virtual void Dispose(bool disposing)
         {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _writer?.Dispose();
+                    _writer = null;
+                }
+                disposedValue = true;
+            }
         }
 
-        /// <summary>
-        /// Creates an instance of the Splunk TCP Sink
-        /// </summary>
-        /// <param name="hostAddress">The Splunk Host</param>
-        /// <param name="port">The TCP port configured in Splunk</param>
-        /// <param name="formatProvider">Optional format provider</param>
-        /// <param name="renderTemplate">If true, the message template will be rendered</param>
-        [Obsolete("Use the overload accepting a connection info object instead. This overload will be removed.", false)]
-        public TcpSink(
-            IPAddress hostAddress,
-            int port,
-            IFormatProvider formatProvider = null,
-            bool renderTemplate = true) : this(new SplunkTcpSinkConnectionInfo(hostAddress, port), formatProvider, renderTemplate)
+        /// <inheritdoc/>
+        public void Dispose()
         {
-        }
-
-        private static TcpSocketWriter CreateSocketWriter(SplunkTcpSinkConnectionInfo connectionInfo)
-        {
-            var reconnectionPolicy = new ExponentialBackoffTcpReconnectionPolicy();
-
-            return new TcpSocketWriter(connectionInfo.Host, connectionInfo.Port, reconnectionPolicy, connectionInfo.MaxQueueSize);
-        }
-
-        private static SplunkJsonFormatter CreateDefaultFormatter(IFormatProvider formatProvider, bool renderTemplate)
-        {
-            return new SplunkJsonFormatter(renderTemplate, formatProvider);
+            Dispose(true);
         }
 
         /// <inheritdoc/>
@@ -116,62 +87,16 @@ namespace Serilog.Sinks.Splunk
             _writer.Enqueue(sb.ToString());
         }
 
-        /// <inheritdoc/>
-        public void Dispose()
+        private static TcpSocketWriter CreateSocketWriter(SplunkTcpSinkConnectionInfo connectionInfo)
         {
-            _writer?.Dispose();
-            _writer = null;
+            var reconnectionPolicy = new ExponentialBackoffTcpReconnectionPolicy();
+
+            return new TcpSocketWriter(connectionInfo.Host, connectionInfo.Port, reconnectionPolicy, connectionInfo.MaxQueueSize);
         }
-    }
 
-
-    /// <summary>
-    /// Defines connection info used to connect against Splunk
-    /// using TCP.
-    /// </summary>
-    public class SplunkTcpSinkConnectionInfo
-    {
-        /// <summary>
-        /// Default size of the socket writer queue.
-        /// </summary>
-        public const int DefaultMaxQueueSize = 10000;
-
-        /// <summary>
-        /// Splunk host.
-        /// </summary>
-        public IPAddress Host { get; }
-
-        /// <summary>
-        /// Splunk port.
-        /// </summary>
-        public int Port { get; }
-
-        /// <summary>
-        /// Max Queue size for the TCP socket writer.
-        /// See <see cref="DefaultMaxQueueSize"/> for default value (10000).
-        /// </summary>
-        public int MaxQueueSize { get; set; } = DefaultMaxQueueSize;
-
-        /// <summary>
-        /// Creates an instance of <see cref="SplunkTcpSinkConnectionInfo"/> used
-        /// for defining connection info for connecting using TCP against Splunk.
-        /// </summary>
-        /// <param name="host">Splunk host.</param>
-        /// <param name="port">Splunk TCP port.</param>
-        public SplunkTcpSinkConnectionInfo(string host, int port) : this(IPAddress.Parse(host), port) { }
-
-        /// <summary>
-        /// Creates an instance of <see cref="SplunkTcpSinkConnectionInfo"/> used
-        /// for defining connection info for connecting using TCP against Splunk.
-        /// </summary>
-        /// <param name="host">Splunk host.</param>
-        /// <param name="port">Splunk TCP port.</param>
-        public SplunkTcpSinkConnectionInfo(IPAddress host, int port)
+        private static SplunkJsonFormatter CreateDefaultFormatter(IFormatProvider formatProvider, bool renderTemplate)
         {
-            Host = host;
-            Port = port;
+            return new SplunkJsonFormatter(renderTemplate, formatProvider);
         }
     }
 }
-
-#endif
