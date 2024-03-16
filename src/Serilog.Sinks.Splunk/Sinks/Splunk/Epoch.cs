d1bce249f6f9e64665e53dfa90cfafdb1b1564ad
EEParker <EEParker@users.noreply.github.com>
2024-03-16 17:06:59 -0500

    Add unit tests and update documentation

--- src/Serilog.Sinks.Splunk/Sinks/Splunk/Epoch.cs
+++ src/Serilog.Sinks.Splunk/Sinks/Splunk/Epoch.cs
@@ -16,17 +16,26 @@ namespace Serilog.Sinks.Splunk
 {
     using System;
 
-    internal static class EpochExtensions
+    /// <summary>
+    /// Provides extension methods for DateTimeOffset to convert to epoch time.
+    /// </summary>
+    public static class EpochExtensions
     {
         private static DateTimeOffset Epoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
 
+        /// <summary>
+        /// Converts a DateTimeOffset value to epoch time with specified sub-second precision.
+        /// </summary>
+        /// <param name="value">The DateTimeOffset value to convert.</param>
+        /// <param name="subSecondPrecision">The precision of sub-second time measurements.</param>
+        /// <returns>The epoch time representation of the DateTimeOffset value.</returns>
         public static double ToEpoch(this DateTimeOffset value, SubSecondPrecision subSecondPrecision = SubSecondPrecision.Milliseconds)
         {
             // From Splunk HTTP Collector Protocol
             // The default time format is epoch time format, in the format <sec>.<ms>. 
             // For example, 1433188255.500 indicates 1433188255 seconds and 500 milliseconds after epoch, 
             // or Monday, June 1, 2015, at 7:50:55 PM GMT.
-            // See: http://dev.splunk.com/view/SP-CAAAE6P
+            // See: https://docs.splunk.com/Documentation/Splunk/9.2.0/SearchReference/Commontimeformatvariables
 
             return Math.Round((value - Epoch).TotalSeconds, (int)subSecondPrecision, MidpointRounding.AwayFromZero);
         }
--- test/Serilog.Sinks.Splunk.Tests/SplunkJsonFormatterTests.cs
+++ test/Serilog.Sinks.Splunk.Tests/SplunkJsonFormatterTests.cs
@@ -10,6 +10,7 @@ using System.IO;
 using Xunit;
 namespace Serilog.Sinks.Splunk.Tests
 {
+
     public class SplunkJsonFormatterTests
     {
         void AssertValidJson(Action<ILogger> act,
