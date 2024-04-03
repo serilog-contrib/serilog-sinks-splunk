9537e3bf99d3e179b0aca0f1091ebd252dc12320
EEParker <EEParker@users.noreply.github.com>
2024-04-02 22:08:13 -0500

    work on build for new project format

--- Build.ps1
+++ Build.ps1
@@ -12,22 +12,35 @@ if(Test-Path .\artifacts) {
 	Remove-Item ./artifacts -Force -Recurse
 }
 
-& dotnet restore --no-cache
+& dotnet restore .\serilog-sinks-splunk.sln --no-cache
 
-$branch = @{ $true = $env:CI_TARGET_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$NULL -ne $env:CI_TARGET_BRANCH];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:CI_BUILD_NUMBER, 10); $false = "local" }[$NULL -ne $env:CI_BUILD_NUMBER];
-$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)) -replace '([^a-zA-Z0-9\-]*)', '')-$revision"}[$branch -eq "master" -and $revision -ne "local"]
+$branch = $NULL -ne $env:CI_TARGET_BRANCH ?  $env:CI_TARGET_BRANCH : (git symbolic-ref --short -q HEAD)
+$revision = $NULL -ne $env:CI_BUILD_NUMBER ? "{0:00000}" -f [Convert]::ToInt32("0" + $env:CI_BUILD_NUMBER, 10) : "local"
+
+# add a suffix if this is not a tag build
+$suffix = $NULL -ne $env:CI_COMMIT_TAG ? "" : "$($branch.Substring(0, [Math]::Min(10,$branch.Length)) -replace '([^a-zA-Z0-9\-]*)', '')-$revision"
+$prefix = $NULL -ne $env:CI_COMMIT_TAG ? $env:CI_COMMIT_TAG : $NULL
+
+Write-Output $brach
+Write-Output $revision
+Write-Output $suffix
 
 Write-Output "build: Package version suffix is $suffix"
+
 
 foreach ($src in Get-ChildItem src/*) {
     Push-Location $src
 
 	Write-Output "build: Packaging project in $src"
 
-    if ($suffix) {
+    if ($prefix) {
+        # release build
+        & dotnet pack -c Release -o ../../artifacts --version-prefix=$prefix
+    } elseif ($suffix) {
+        # prerelease build
         & dotnet pack -c Release -o ../../artifacts --version-suffix=$suffix
     } else {
+        # local build
         & dotnet pack -c Release -o ../../artifacts
     }
     if($LASTEXITCODE -ne 0) { throw "Packaging failed" }
@@ -36,7 +49,7 @@ foreach ($src in Get-ChildItem src/*) {
 }
 
 Write-Output "build: Checking complete solution builds"
-& dotnet build
+& dotnet build .\serilog-sinks-splunk.sln -c Release
 if($LASTEXITCODE -ne 0) { throw "Solution build failed" }
 
 
@@ -53,13 +66,14 @@ foreach ($test in Get-ChildItem test/*.Tests) {
 
 Pop-Location
 
-if ($env:NUGET_API_KEY) {
+if ($env:NUGET_API_KEY -and ($NULL -ne $suffix -or $NULL -ne $prefix)) {
     # GitHub Actions will only supply this to branch builds and not PRs. We publish
     # builds from any branch this action targets (i.e. master and dev).
-    
+
     Write-Output "build: Publishing NuGet packages"
-    
+
     foreach ($nupkg in Get-ChildItem artifacts/*.nupkg) {
+        Write-Output "build: Publishing $nupkg"
         & dotnet nuget push -k $env:NUGET_API_KEY -s https://api.nuget.org/v3/index.json "$nupkg" --no-symbols
         if($LASTEXITCODE -ne 0) { throw "Publishing failed" }
     }
