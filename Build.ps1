55d211c7d2c1c019c6fa46b5fc9e9f0209fc29ca
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-04-03 14:05:33 +1000

    Set up GitHub Actions (#174)
    * Enable GitHub Actions
    
    * Main branch is called master in this repo
    
    * Ignore .idea
    
    * No need to offset builds by 200
    
    * work on build for new project format
    
    * add automatic release notes
    
    ---------
    
    Co-authored-by: EEParker <EEParker@users.noreply.github.com>

--- Build.ps1
+++ Build.ps1
@@ -1,51 +1,80 @@
-$ErrorActionPreference = "Stop"
-echo "build: Build started"
+Write-Output "build: Build started"
 
 Push-Location $PSScriptRoot
 
+Write-Output "build: Tool versions follow"
+
+dotnet --version
+dotnet --list-sdks
+
 if(Test-Path .\artifacts) {
-	echo "build: Cleaning .\artifacts"
-	Remove-Item .\artifacts -Force -Recurse
+	Write-Output "build: Cleaning ./artifacts"
+	Remove-Item ./artifacts -Force -Recurse
 }
 
-& dotnet restore --no-cache
+& dotnet restore .\serilog-sinks-splunk.sln --no-cache
 
-$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
-$commitHash = $(git rev-parse --short HEAD)
-$buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
+$branch = $NULL -ne $env:CI_TARGET_BRANCH ?  $env:CI_TARGET_BRANCH : (git symbolic-ref --short -q HEAD)
+$revision = $NULL -ne $env:CI_BUILD_NUMBER ? "{0:00000}" -f [Convert]::ToInt32("0" + $env:CI_BUILD_NUMBER, 10) : "local"
 
-echo "build: Package version suffix is $suffix"
-echo "build: Build version suffix is $buildSuffix" 
+# add a suffix if this is not a tag build
+$suffix = $NULL -ne $env:CI_COMMIT_TAG ? "" : "$($branch.Substring(0, [Math]::Min(10,$branch.Length)) -replace '([^a-zA-Z0-9\-]*)', '')-$revision"
+$prefix = $NULL -ne $env:CI_COMMIT_TAG ? $env:CI_COMMIT_TAG : $NULL
 
-foreach ($src in ls src/*) {
+Write-Output $brach
+Write-Output $revision
+Write-Output $suffix
+
+Write-Output "build: Package version suffix is $suffix"
+
+
+foreach ($src in Get-ChildItem src/*) {
     Push-Location $src
 
-	echo "build: Packaging project in $src"
+	Write-Output "build: Packaging project in $src"
 
-    & dotnet build -c Release --version-suffix=$buildSuffix
-    if ($suffix) {
-        & dotnet pack -c Release --include-symbols -o ..\..\artifacts --version-suffix=$suffix --no-build
+    if ($prefix) {
+        # release build
+        & dotnet pack -c Release -o ../../artifacts --version-prefix=$prefix
+    } elseif ($suffix) {
+        # prerelease build
+        & dotnet pack -c Release -o ../../artifacts --version-suffix=$suffix
     } else {
-        & dotnet pack -c Release --include-symbols -o ..\..\artifacts --no-build
+        # local build
+        & dotnet pack -c Release -o ../../artifacts
     }
-    if($LASTEXITCODE -ne 0) { exit 1 }    
+    if($LASTEXITCODE -ne 0) { throw "Packaging failed" }
 
     Pop-Location
 }
 
-foreach ($test in ls test/*.Tests) {
+Write-Output "build: Checking complete solution builds"
+& dotnet build .\serilog-sinks-splunk.sln -c Release
+if($LASTEXITCODE -ne 0) { throw "Solution build failed" }
+
+
+foreach ($test in Get-ChildItem test/*.Tests) {
     Push-Location $test
 
-	echo "build: Testing project in $test"
+	Write-Output "build: Testing project in $test"
 
     & dotnet test -c Release
-    if($LASTEXITCODE -ne 0) { exit 3 }
+    if($LASTEXITCODE -ne 0) { throw "Testing failed" }
 
     Pop-Location
 }
 
-dotnet build -c Release .\sample\Sample\Sample.csproj
-
 Pop-Location
+
+if ($env:NUGET_API_KEY -and ($NULL -ne $suffix -or $NULL -ne $prefix)) {
+    # GitHub Actions will only supply this to branch builds and not PRs. We publish
+    # builds from any branch this action targets (i.e. master and dev).
+
+    Write-Output "build: Publishing NuGet packages"
+
+    foreach ($nupkg in Get-ChildItem artifacts/*.nupkg) {
+        Write-Output "build: Publishing $nupkg"
+        & dotnet nuget push -k $env:NUGET_API_KEY -s https://api.nuget.org/v3/index.json "$nupkg" --no-symbols
+        if($LASTEXITCODE -ne 0) { throw "Publishing failed" }
+    }
+}
